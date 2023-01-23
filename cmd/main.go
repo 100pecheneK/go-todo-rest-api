44f412bd72b32a3b086eabb83a40364c8cd031a0
100pecheneK <mistermihail23@gmail.com>
2023-01-23 15:15:07 +0500

    add viper config and update server port form viper config

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,28 @@ import (
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/handler"
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/repository"
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
-	if err := srv.Run("3000", handlers.InitRoutes()); err != nil {
+	port := viper.GetString("port")
+	log.Println("starting server on port:", port)
+	if err := srv.Run(port, handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
