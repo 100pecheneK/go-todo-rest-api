908176382a94a7be0de83cdb1db9a2ca088505ed
100pecheneK <mistermihail23@gmail.com>
2023-01-23 19:02:17 +0500

    add postgres connection to server

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/100pecheneK/go-todo-rest-api.git/internal/server"
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/handler"
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/repository"
 	"github.com/100pecheneK/go-todo-rest-api.git/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
 
@@ -14,8 +17,24 @@ func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("psql.host"),
+		Port:     viper.GetString("psql.port"),
+		Username: viper.GetString("psql.username"),
+		Password: os.Getenv("PSQL_PASSWORD"),
+		DBName:   viper.GetString("psql.DBName"),
+		SSLMode:  viper.GetString("psql.SSLMode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	} else {
+		log.Println("db connect successfull")
+	}
 
-	repos := repository.NewRepository()
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
